a5fc0617f6edf0f569a3d7522ef21033eff7b20b
Vijay Bahadur <vijaybahadur9494@gmail.com>
2026-02-01 17:43:54 +0530

    jkfsfsfdffni

---
+++ day2/dataType.js
@@ -0,0 +1,22 @@
+
+let day = "sunday";
+console.log(typeof day); // string
+
+let temperature = 25;
+console.log(typeof temperature); // number
+
+let dayEnum = ["sunday", "monday", "tuesday"];
+console.log(typeof dayEnum); // object
+
+let isSunny = true;
+console.log(typeof isSunny); // boolean
+
+let weather = { day: "sunday", temperature: 25 };
+console.log(typeof weather); // object
+let nothing = null;
+console.log(typeof nothing); // object
+let notDefined;
+console.log(typeof notDefined); // undefined
+
+let bigNumber = 9007199254740991n;
+console.log(typeof bigNumber); // bigint
